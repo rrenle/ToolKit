7e38805e8f374bf2aeb98800f3ac4e263fede27b
Iterfqly <53036047+Iterfqly@users.noreply.github.com>
2023-06-27 10:58:28 +0800

    Add files via upload

---
+++ models/transformer_module.py
@@ -0,0 +1,155 @@
+import torch
+import torch.nn.functional as F
+from torch import nn, Tensor
+
+import copy
+from typing import Optional
+
+def _get_activation_fn(activation):
+    """Return an activation function given a string"""
+    if activation == "relu":
+        return F.relu
+    if activation == "gelu":
+        return F.gelu
+    if activation == "glu":
+        return F.glu
+    raise RuntimeError(F"activation should be relu/gelu, not {activation}.")
+
+
+class TransformerEncoderLayer(nn.Module):
+    """ Modified from https://github.com/facebookresearch/detr/blob/master/models/transformer.py"""
+
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1,
+                 activation="relu", normalize_before=False, return_atten_map=False, ffn=False):
+        super().__init__()
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout, batch_first=True)
+        self.dropout1 = nn.Dropout(dropout)
+        self.ffn = ffn
+
+        if self.ffn:
+            self.linear1 = nn.Linear(d_model, dim_feedforward)
+            self.dropout = nn.Dropout(dropout)
+            self.linear2 = nn.Linear(dim_feedforward, d_model)
+            self.norm1 = nn.LayerNorm(d_model)
+            self.norm2 = nn.LayerNorm(d_model)
+            self.dropout2 = nn.Dropout(dropout)
+            self.activation = _get_activation_fn(activation)
+            
+        self.normalize_before = normalize_before
+        self.return_atten_map = return_atten_map
+
+    def with_pos_embed(self, tensor, pos: Optional[Tensor]):
+        return tensor if pos is None else tensor + pos
+
+    def forward_post(self,
+                     src,
+                     src_mask: Optional[Tensor] = None,
+                     src_key_padding_mask: Optional[Tensor] = None,
+                     pos: Optional[Tensor] = None):
+        q = k = self.with_pos_embed(src, pos)
+        if self.return_atten_map:
+            src2, att_map = self.self_attn(q, k, value=src,
+                                           attn_mask=src_mask,
+                                           key_padding_mask=src_key_padding_mask)
+        else:
+            src2 = self.self_attn(q, k, value=src, attn_mask=src_mask,
+                                  key_padding_mask=src_key_padding_mask)[0]
+        src = src + self.dropout1(src2)
+
+        if self.ffn:
+            src = self.norm1(src)
+            src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
+            src = src + self.dropout2(src2)
+            src = self.norm2(src)
+
+        if self.return_atten_map:
+            return src, att_map
+        else:
+            return src
+
+    def forward_pre(self, src,
+                    src_mask: Optional[Tensor] = None,
+                    src_key_padding_mask: Optional[Tensor] = None,
+                    pos: Optional[Tensor] = None):
+        src2 = self.norm1(src)
+        q = k = self.with_pos_embed(src2, pos)
+        if self.return_atten_map:
+            src2, att_map = self.self_attn(q, k, value=src,
+                                           attn_mask=src_mask,
+                                           key_padding_mask=src_key_padding_mask)
+        else:
+            src2 = self.self_attn(q, k, value=src, attn_mask=src_mask,
+                                  key_padding_mask=src_key_padding_mask)[0]
+        src = src + self.dropout1(src2)
+        src2 = self.norm2(src)
+        src2 = self.linear2(self.dropout(self.activation(self.linear1(src2))))
+        src = src + self.dropout2(src2)
+        if self.return_atten_map:
+            return src, att_map
+        else:
+            return src
+
+    def forward(self, src,
+                src_mask: Optional[Tensor] = None,
+                src_key_padding_mask: Optional[Tensor] = None,
+                pos: Optional[Tensor] = None):
+        if self.normalize_before:
+            return self.forward_pre(src, src_mask, src_key_padding_mask, pos)
+        return self.forward_post(src, src_mask, src_key_padding_mask, pos)
+
+
+def _get_clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, num_layers, d_model, n_head=1, dim_feedforward=128,
+                 norm=None, pe_only_at_begin=False, return_atten_map=False):
+        super().__init__()
+        
+        encoder_layer = TransformerEncoderLayer(
+            d_model=d_model, 
+            nhead=n_head, 
+            dim_feedforward=dim_feedforward, 
+            activation='relu',
+            return_atten_map=False,
+            ffn=True
+        )
+
+        self.layers = _get_clones(encoder_layer, num_layers)
+        self.norm = norm
+        self.pe_only_at_begin = pe_only_at_begin
+        self.return_atten_map = return_atten_map
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+
+    def forward(self, src,
+                mask: Optional[Tensor] = None,
+                src_key_padding_mask: Optional[Tensor] = None,
+                pos: Optional[Tensor] = None):
+        output = src
+        atten_maps_list = []
+        for layer in self.layers:
+            if self.return_atten_map:
+                output, att_map = layer(output, src_mask=mask, pos=pos,
+                                        src_key_padding_mask=src_key_padding_mask)
+                atten_maps_list.append(att_map)
+            else:
+                output = layer(output, src_mask=mask,  pos=pos,
+                               src_key_padding_mask=src_key_padding_mask)
+
+            # only add position embedding to the first atttention layer
+            pos = None if self.pe_only_at_begin else pos
+
+        if self.norm is not None:
+            output = self.norm(output)
+
+        if self.return_atten_map:
+            return output, torch.stack(atten_maps_list)
+        else:
+            return output
+
